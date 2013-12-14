73a95a63adf75d2ed7150f1c2af86cfd0e360f27
trashy111 <bradleym007@gmail.com>
2013-12-14 14:24:56 -0800

    Revert "Revert "Brought over everything to new branch""
    This reverts commit 6420fab02e05489ae5f7cab1e3526eb725f73721.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-function draw()
+var x
+var y
+
+function draw(x,y)
 {
 var canvas = document.getElementById('gameCanvas');
 var ctx = canvas.getContext('2d');
+
 ctx.fillStyle="#FF0000";
-ctx.fillRect(0,0,150,75);
+ctx.fillRect(x,y,20,75);
 }
+
