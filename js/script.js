4d57333bdfd24e0eed116cd46b5a4c3c8a0468df
luke738 <luke7380@gmail.com>
2013-12-14 14:46:50 -0800

    yo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
-var x
-var y
-
-function draw(x,y)
-{
-var canvas = document.getElementById('gameCanvas');
-var ctx = canvas.getContext('2d');
-
-ctx.fillStyle="#FF0000";
-ctx.fillRect(x,y,20,75);
+function draw() {
+	var canvas = document.getElementById("gameCanvas");
+	var ctx = canvas.getContext("2d");
+	var img = new Image();
+	
+	//ctx.fillStyle = "rgba(255,255,0,0.5)";
+	//ctx.fillRect (100, 50, 550, 500);
+	//ctx.fillStyle = "rgba(0,255,255,0.5)";
+	//ctx.fillRect (200, 100, 550, 500);
+	//ctx.fillStyle = "rgba(255,0,255,0.5)";
+	//ctx.fillRect (300, 150, 550, 500);
+	//ctx.clearRect (350, 200, 250, 300);
+	for (var h=0; h<20; h++) {
+		for (var i=0; i<20; i++) {
+			ctx.fillStyle = "rgba(" + 0 + "," + 10*i + "," + 10*h + ",0.5)";
+			ctx.fillRect (25*h, 25*i, 25, 25);
+		}
+	}
+	
+	img.onload = function(){
+		//for (var i = 0; i<10; i++) {
+			ctx.drawImage(img,32,50);
+		//}
+	}	
+	img.src = "assets/images/bullet.png";
 }
-
