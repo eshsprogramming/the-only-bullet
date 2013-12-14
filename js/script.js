422e630a209713f1f70aedbcaf5fe578e64e7266
MaxWofford <max.speed.wofford@gmail.com>
2013-12-14 15:05:50 -0800

    Added new bullet asset

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-var x
-var y
+//var x
+//var y
 
-function draw(x,y)
+function draw()
 {
 var canvas = document.getElementById('gameCanvas');
 var ctx = canvas.getContext('2d');
-
 ctx.fillStyle="#FF0000";
-ctx.fillRect(x,y,20,75);
+ctx.fillRect(0,0,20,75);
+var img=document.getElementById("/assets/images/testPotato.jpg");
+ctx.drawImage(img,10,10);
 }
 
