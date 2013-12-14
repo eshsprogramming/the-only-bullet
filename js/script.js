e3d984c48b60d48503e07b93900c5ef96577a2df
luke738 <luke7380@gmail.com>
2013-12-14 15:58:07 -0800

    Merge

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
+//var start = 0;
 
-function draw() {
+function init() {
+	//var img = new Image();
+	//img.src = "assets/images/bullet.png";
+	requestAnimationFrame(draw);
+}
+
+function draw(timestamp) {
+	var img = new Image();
+	img.src = "assets/images/bullet.png";
 	var canvas = document.getElementById("gameCanvas");
 	var ctx = canvas.getContext("2d");
-	var img = new Image();
+	//var start = 0;
+	/*if (start===0) { 
+		start = timestamp;
+	}
+	var x = -(timestamp-start);*/
+	console.log(timestamp/1000);
+	//console.log(start);
+	
+	ctx.clearRect(0,0,1000,700);
+	ctx.fillRect(0,timestamp/100,25,25);
 	
 	//ctx.fillStyle = "rgba(255,255,0,0.5)";
 	//ctx.fillRect (100, 50, 550, 500);
@@ -13,15 +31,15 @@ function draw() {
 	//ctx.clearRect (350, 200, 250, 300);
 	for (var h=0; h<20; h++) {
 		for (var i=0; i<20; i++) {
-			ctx.fillStyle = "rgba(" + 0 + "," + 10*i + "," + 10*h + ",0.5)";
+			ctx.fillStyle = "rgba(" + 0 + "," + 12*i + "," + 12*h + ",0.5)";
 			ctx.fillRect (25*h, 25*i, 25, 25);
 		}
 	}
 	
-	img.onload = function(){
-		//for (var i = 0; i<10; i++) {
-			ctx.drawImage(img,32,50);
-		//}
-	}	
-	img.src = "assets/images/bullet.png";
+	img.onload = function() {
+		ctx.drawImage(img,50,50);
+	}
+	//console.log(x);
+	
+	requestAnimationFrame(draw);
 }
