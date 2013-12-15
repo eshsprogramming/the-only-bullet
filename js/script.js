10b5a492a0a04510b3b61af87d7a2c722ab58003
luke738 <luke7380@gmail.com>
2013-12-14 19:25:06 -0800

    Adds a moving bullet

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 //var start = 0;
-
+var img = new Image();
 function init() {
 	//var img = new Image();
-	//img.src = "assets/images/bullet.png";
-	requestAnimationFrame(draw);
+	img.src = "assets/images/bullet2.png";
+	img.onload = function() {
+		requestAnimationFrame(draw);
+	}
 }
 
 function draw(timestamp) {
-	var img = new Image();
-	img.src = "assets/images/bullet.png";
 	var canvas = document.getElementById("gameCanvas");
 	var ctx = canvas.getContext("2d");
 	//var start = 0;
@@ -22,13 +22,6 @@ function draw(timestamp) {
 	ctx.clearRect(0,0,1000,700);
 	ctx.fillRect(0,timestamp/100,25,25);
 	
-	//ctx.fillStyle = "rgba(255,255,0,0.5)";
-	//ctx.fillRect (100, 50, 550, 500);
-	//ctx.fillStyle = "rgba(0,255,255,0.5)";
-	//ctx.fillRect (200, 100, 550, 500);
-	//ctx.fillStyle = "rgba(255,0,255,0.5)";
-	//ctx.fillRect (300, 150, 550, 500);
-	//ctx.clearRect (350, 200, 250, 300);
 	for (var h=0; h<20; h++) {
 		for (var i=0; i<20; i++) {
 			ctx.fillStyle = "rgba(" + 0 + "," + 12*i + "," + 12*h + ",0.5)";
@@ -36,10 +29,8 @@ function draw(timestamp) {
 		}
 	}
 	
-	img.onload = function() {
-		ctx.drawImage(img,50,50);
-	}
-	//console.log(x);
 	
+	ctx.drawImage(img,timestamp/100,0);
+	//console.log(x);
 	requestAnimationFrame(draw);
 }
