9d4a5e6a8151e1063ce76bc3d7d2718d30a6986e
luke738 <luke7380@gmail.com>
2013-12-15 17:25:14 -0800

    Adds bullets, a broken controllable bullet, and physics
    We went back to Corona!

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
--- Project: ludem-dare-28
+-- Project: ludum-dare-28
 -- Description:
 --
 -- Version: 1.0
@@ -8,37 +8,53 @@
 ---- cpmgen main.lua
 
 
---[[local sprite = require("sprite")
+--local sprite = require("sprite")
 
 local physics = require("physics")
 physics.start()
 
-system.activate("multitouch")]]
+--system.activate("multitouch")
 
-local sheetData =
+--[[local sheetData =
 {
 	width = 32,
 	height = 16,
-	numFrames = 19
+	numFrames = 1
 }
 
-local mySheet = graphics.newImageSheet( "bullet.png", sheetData )
+local mySheet = graphics.newImageSheet( "assets/images/bullet.png", sheetData )
 
 local sequenceData = {
 
   {name = "normalRun",
    start = 1,
-   count = 19,
+   count = 1,
    time = 1250,
    loopCount = 0,
    loopDirection = "forward"
    }
-}
-local animation = display.newSprite(mySheet, sequenceData)
-animation.x = display.contentWidth/2
-animation.y = display.contentHeight/2
+}]]
+local floor = display.newImage("assets/images/floortest.png", 150, 250)
+physics.addBody(floor, "static", {friction=0.5})
 
-animation:play()
+local bulletTarget = {}
+for i=1,10 do
+	for j=1,10 do
+		bulletTarget[i] = display.newImage("assets/images/bullet.png", 20*i+50, 20*j)
+		physics.addBody( bulletTarget[i], { density=0.2, friction=0.1, bounce=0.5 } )
+		--bulletTarget[i]:applyLinearImpulse(math.random(-i,i),i,bulletTarget[i].x,bulletTarget[i].y)
+	end
+end
+
+local bullet = display.newImage("assets/images/bullet.png", 150, 50)
+physics.addBody(bullet, {density=0.5, friction=0.1, bounce=0.25})
+bullet.isBullet = true
+math.randomseed(os.time())
+bullet:applyLinearImpulse(math.random(-10,10),10,bullet.x,bullet.y)
+--animation.x = display.contentWidth/2
+--animation.y = display.contentHeight/2
+
+--animation:play()
 
 --[[local function myTouchListener( event )
   if event.x > display.contentWidth/2 then
@@ -46,17 +62,17 @@ animation:play()
   else
     animation.x = animation.x - 5
   end
-end--]]
+end]]
 
 local function myTouchListener( event )
-  animation.x = event.x
-  animation.y = event.y
+  bullet.x = event.x
+  bullet.y = event.y
 end
 
---animation.x = ..event.x
---animation.y = ...event.y
+--[[animation.x = ..event.x
+animation.y = ...event.y
 
---local myButton = display.newRect( display.contentWidth/2, 100, 200, 50 )
+local myButton = display.newRect( display.contentWidth/2, 100, 200, 50 )]]
 Runtime:addEventListener( "touch", myTouchListener )
 
 
